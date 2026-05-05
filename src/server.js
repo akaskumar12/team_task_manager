a271c8adbd3cde129bbc37f3801e874283bb1574
Akash Kumar <akashkhekra@gmail.com>
2026-05-05 12:27:19 +0530

    initial commit

--- src/server.js
+++ src/server.js
@@ -51,7 +51,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(MONGO_URI)
   .then(() => {
-  cors());    console.log("✅ MongoDB connected");
+    console.log("✅ MongoDB connected");
     app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
   })
   .catch((err) => {
